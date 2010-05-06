568377ee5d59517a90ecad5a73a522fe709fe730
Amboy Suarez <amboysuareziii@gmail.com>
2010-05-06 09:50:21 +0800

    updated index.html.erb of article part 2, nilagay ko na lahat sa helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,13 +1,42 @@
 module ArticlesHelper
   def view_legend
     %Q{
-      <tr>
-        <td style="width: 10px; background-color: 	#B7410E"> </td>
-        <td>Draft</td>
-        <td> </td>
-        <td style="width: 10px; background-color: #E0115F"> </td>
-        <td>Posted</td>
-    </tr>
+     <table style="float:right; text-align: center">
+       <tr>
+         <td style="width: 10px; background-color: 	#B7410E"> </td>
+         <td>Draft</td>
+         <td> </td>
+         <td style="width: 10px; background-color: #E0115F"> </td>
+         <td>Posted</td>
+       </tr>
+     </table>
+    }
+  end
+  def list_all(article, status, author)
+    %Q{
+      <table class="all">
+        <tr class = #{status}>
+          <th class="Labels">Title: </th>
+          <td> #{link_to article.title, article} </td>
+        </tr>
+
+        <tr class = #{status}>
+          <th class="Labels">Author: </th>
+          <td>#{link_to author.full_name, author_path(author)}</td>
+        </tr>
+
+        <tr class = #{status}>
+          <th class="Labels">Actions:</th>
+          <td >
+            <ul class="nobullets">
+              <li> #{link_to "Edit", edit_article_path(article) if !article.is_posted? }</li>
+              <li> #{link_to "Delete", article, :confirm => "Confirm delete? \'#{article.title}\'", :method => :delete}</li>
+            </ul>
+          </td>
+        </tr>
+
+        <tr><td colspan=2><hr /></td></tr>
+      </table>
     }
   end
   def view_comments(article)
