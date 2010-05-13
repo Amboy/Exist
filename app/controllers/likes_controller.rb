0b1d27085723c1f292ab4e4b8da0430bbb511807
Amboy Suarez <amboysuareziii@gmail.com>
2010-05-13 17:50:11 +0800

    modified like link, will not create again if na like na :3

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,8 @@
 class LikesController < ApplicationController
   def ilike
     @article = Article.find(params[:article_id])
-    @like = Like.create(:author => current_author, :article => @article)
+    @like = Like.create(:author => current_author, :article => @article) unless Like.find(:first, :conditions => {:author_id => current_author.id, :article_id => @article.id})
+    
     render :partial => "like", :locals =>{:article => @article, :like => @like}
   end
   def unlike
